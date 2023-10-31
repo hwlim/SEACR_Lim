261c9a217d42756290c3577633f7a9b065b580a8
Hee Woong Lim <bellelavie@gmail.com>
2023-10-31 14:00:34 -0400

    new line added for awk command

--- SEACR_1.3.sh
+++ SEACR_1.3.sh
@@ -11,13 +11,20 @@ then
 	
 	Description of input fields:
 	
-	Field 1: Target data bedgraph file in UCSC bedgraph format (https://genome.ucsc.edu/goldenpath/help/bedgraph.html) that omits regions containing 0 signal.
+	Field 1: Target data bedgraph file in UCSC bedgraph format
+	(https://genome.ucsc.edu/goldenpath/help/bedgraph.html) that omits regions containing 0 signal.
 	
-	Field 2: Control (IgG) data bedgraph file to generate an empirical threshold for peak calling. Alternatively, a numeric threshold n between 0 and 1 returns the top n fraction of peaks based on total signal within peaks.
+	Field 2: Control (IgG) data bedgraph file to generate an empirical threshold for peak calling.
+	Alternatively, a numeric threshold n between 0 and 1 returns the top n fraction of peaks
+	based on total signal within peaks.
 	
-	Field 3: “norm” denotes normalization of control to target data, “non” skips this behavior. "norm" is recommended unless experimental and control data are already rigorously normalized to each other (e.g. via spike-in).
+	Field 3: “norm” denotes normalization of control to target data, “non” skips this behavior.
+	"norm" is recommended unless experimental and control data are already rigorously normalized
+	to each other (e.g. via spike-in).
 		
-	Field 4: “relaxed” uses a total signal threshold between the knee and peak of the total signal curve, and corresponds to the “relaxed” mode described in the text, whereas “stringent” uses the peak of the curve, and corresponds to “stringent” mode.
+	Field 4: “relaxed” uses a total signal threshold between the knee and peak of the total signal curve,
+	and corresponds to the “relaxed” mode described in the text,
+	whereas “stringent” uses the peak of the curve, and corresponds to “stringent” mode.
 	
 	Field 5: Output prefix
 	
@@ -39,7 +46,8 @@ then
 	
 	Field 5: Maximum bedgraph signal attained at any base pair within denoted coordinates
 	
-	Field 6: Region representing the farthest upstream and farthest downstream bases within the denoted coordinates that are represented by the maximum bedgraph signal
+	Field 6: Region representing the farthest upstream and farthest downstream bases
+	within the denoted coordinates that are represented by the maximum bedgraph signal
 	
 	Examples:
 	bash SEACR_1.3.sh target.bedgraph IgG.bedgraph norm stringent output
@@ -98,16 +106,92 @@ fi
 
 echo "Creating experimental AUC file: $(date)"
 
-awk 'BEGIN{s=1}; {if(s==1){s++}else if(s==2){if($4 > 0){chr=$1; start=$2; stop=$3; max=$4; coord=$1":"$2"-"$3; auc=$4*($3-$2); num=1; s++}}else{if($4 > 0){if(chr==$1 && $2==stop){num++; stop=$3; auc=auc+($4*($3-$2)); if ($4 > max){max=$4; coord=$1":"$2"-"$3
-}else if($4 == max){split(coord,t,"-"); coord=t[1]"-"$3}}else{print chr"\t"start"\t"stop"\t"auc"\t"max"\t"coord"\t"num; chr=$1; start=$2; stop=$3; max=$4; coord=$1":"$2"-"$3; auc=$4*($3-$2); num=1}}}}' $1 > $password.auc.bed
+awk 'BEGIN{s=1};
+	{
+		if(s==1){
+			s++
+		}else if(s==2){
+			if($4 > 0){
+				chr=$1;
+				start=$2;
+				stop=$3;
+				max=$4;
+				coord=$1":"$2"-"$3;
+				auc=$4*($3-$2);
+				num=1;
+				s++
+			}
+		}else{
+			if($4 > 0){
+				if(chr==$1 && $2==stop){
+					num++;
+					stop=$3;
+					auc=auc+($4*($3-$2));
+					if ($4 > max){
+						max=$4;
+						coord=$1":"$2"-"$3
+					}else if($4 == max){
+						split(coord,t,"-");
+						coord=t[1]"-"$3
+					}
+				}else{
+					print chr"\t"start"\t"stop"\t"auc"\t"max"\t"coord"\t"num; chr=$1;
+					start=$2;
+					stop=$3;
+					max=$4;
+					coord=$1":"$2"-"$3;
+					auc=$4*($3-$2);
+					num=1
+				}
+			}
+		}
+	}' $1 > $password.auc.bed
 cut -f 4,7 $password.auc.bed > $password.auc
 
 if [[ -f $2 ]]
 then
   echo "Creating control AUC file: $(date)"
 
-  awk 'BEGIN{s=1}; {if(s==1){s++}else if(s==2){if($4 > 0){chr=$1; start=$2; stop=$3; max=$4; coord=$1":"$2"-"$3; auc=$4*($3-$2); num=1; s++}}else{if($4 > 0){if(chr==$1 && $2==stop){num++; stop=$3; auc=auc+($4*($3-$2)); if ($4 > max){max=$4; coord=$1":"$2"-"
-$3}else if($4 == max){split(coord,t,"-"); coord=t[1]"-"$3}}else{print chr"\t"start"\t"stop"\t"auc"\t"max"\t"coord"\t"num; chr=$1; start=$2; stop=$3; max=$4; coord=$1":"$2"-"$3; auc=$4*($3-$2); num=1}}}}' $2 > $password2.auc.bed
+  awk 'BEGIN{s=1};
+  		{
+			if(s==1){
+				s++
+			}else if(s==2){
+				if($4 > 0){
+					chr=$1;
+					start=$2;
+					stop=$3;
+					max=$4;
+					coord=$1":"$2"-"$3;
+					auc=$4*($3-$2);
+					num=1;
+					s++
+				}
+			}else{
+				if($4 > 0){
+					if(chr==$1 && $2==stop){
+						num++;
+						stop=$3;
+						auc=auc+($4*($3-$2));
+						if ($4 > max){
+							max=$4;
+							coord=$1":"$2"-"$3
+						}else if($4 == max){
+							split(coord,t,"-");
+							coord=t[1]"-"$3
+						}
+					}else{
+						print chr"\t"start"\t"stop"\t"auc"\t"max"\t"coord"\t"num; chr=$1;
+						start=$2;
+						stop=$3;
+						max=$4;
+						coord=$1":"$2"-"$3;
+						auc=$4*($3-$2);
+						num=1
+					}
+				}
+			}
+		}' $2 > $password2.auc.bed
   cut -f 4,7 $password2.auc.bed > $password2.auc
 fi
 
@@ -166,9 +250,76 @@ mean=`awk '{s+=$3-$2; t++}END{print s/(t*10)}' $password.auc.threshold.bed`
 
 if [[ -f $2 ]]
 then
-	awk -v value=$mean 'BEGIN{s=1}; {if(s==1){chr=$1; start=$2; stop=$3; auc=$4; max=$5; coord=$6; s++}else{if(chr==$1 && $2 < stop+value){stop=$3; auc=auc+$4; if($5 > max){max=$5; coord=$6}else if($5==max){split(coord,t,"-"); split($6,u,"-"); coord=t[1]"-"u[2]}}else{print chr"\t"start"\t"stop"\t"auc"\t"max"\t"coord; chr=$1; start=$2; stop=$3; auc=$4; max=$5; coord=$6}}}' $password.auc.threshold.bed | bedtools intersect -wa -v -a - -b $password2.auc.threshold.bed > $5.auc.threshold.merge.bed  
+	awk -v value=$mean 'BEGIN{s=1};
+		{
+			if(s==1){
+				chr=$1;
+				start=$2;
+				stop=$3;
+				auc=$4;
+				max=$5;
+				coord=$6;
+				s++
+			}else{
+				if(chr==$1 && $2 < stop+value){
+					stop=$3;
+					auc=auc+$4;
+					if($5 > max){
+						max=$5;
+						coord=$6
+					}else if($5==max){
+						split(coord,t,"-");
+						split($6,u,"-");
+						coord=t[1]"-"u[2]
+					}
+				}else{
+					print chr"\t"start"\t"stop"\t"auc"\t"max"\t"coord;
+					chr=$1;
+					start=$2;
+					stop=$3;
+					auc=$4;
+					max=$5;
+					coord=$6
+				}
+			}
+		}' $password.auc.threshold.bed \
+	| bedtools intersect -wa -v -a - -b $password2.auc.threshold.bed \
+	> $5.auc.threshold.merge.bed  
 else
-	awk -v value=$mean 'BEGIN{s=1}; {if(s==1){chr=$1; start=$2; stop=$3; auc=$4; max=$5; coord=$6; s++}else{if(chr==$1 && $2 < stop+value){stop=$3; auc=auc+$4; if($5 > max){max=$5; coord=$6}else if($5==max){split(coord,t,"-"); split($6,u,"-"); coord=t[1]"-"u[2]}}else{print chr"\t"start"\t"stop"\t"auc"\t"max"\t"coord; chr=$1; start=$2; stop=$3; auc=$4; max=$5; coord=$6}}}' $password.auc.threshold.bed > $5.auc.threshold.merge.bed
+	awk -v value=$mean 'BEGIN{s=1};
+		{
+			if(s==1){
+				chr=$1;
+				start=$2;
+				stop=$3;
+				auc=$4;
+				max=$5;
+				coord=$6;
+				s++
+			}else{
+				if(chr==$1 && $2 < stop+value){
+					stop=$3;
+					auc=auc+$4;
+					if($5 > max){
+						max=$5;
+						coord=$6
+					}else if($5==max){
+						split(coord,t,"-");
+						split($6,u,"-");
+						coord=t[1]"-"u[2]
+					}
+				}else{
+					print chr"\t"start"\t"stop"\t"auc"\t"max"\t"coord;
+					chr=$1;
+					start=$2;
+					stop=$3;
+					auc=$4;
+					max=$5;
+					coord=$6
+				}
+			}
+		}' $password.auc.threshold.bed \
+	> $5.auc.threshold.merge.bed
 fi
 
 if [[ $height == "relaxed" ]]
